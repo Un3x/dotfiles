d3bc00aaffd3a59c80b2f37d120efc97c5e3fd2c
Thomas COMES <thomas.comes.tech@gmail.com>
2026-09-14 13:00:47 +0200

    Stop configuring clangd in nvim; update plugin lockfile

--- nvim/.config/nvim/lua/config/cmp-config.lua
+++ nvim/.config/nvim/lua/config/cmp-config.lua
@@ -28,9 +28,6 @@ require'cmp'.setup {
 }
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
--- The following example advertise capabilities to `clangd`.
-require'lspconfig'.clangd.setup {
-  capabilities = capabilities,
-}
+vim.lsp.config('*', {
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
